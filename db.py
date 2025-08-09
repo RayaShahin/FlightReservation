70a6ee9cd18f95b4344180b420f047ea1cdc710e
Raya Shahin <rayashahin08@gmail.com>
2025-08-09 15:17:54 +0200

    create tables

---
+++ db.py
@@ -0,0 +1,34 @@
+import sqlite3
+from pathlib import Path
+
+db_path = "data/flights.db"
+
+def init_db():
+    Path("data").mkdir(exist_ok=True)
+    with sqlite3.connect(db_path) as conn:
+        c = conn.cursor()       
+        # create flights table
+        c.execute("""
+                  CREATE TABLE IF NOT EXISTS flights(
+                      flight_id INTEGER PRIMARY KEY AUTOINCREMEMT,
+                      flight_number TEXT NOT NULL,
+                      departure TEXT NOT NULL,
+                      arrival TEXT NOT NULL,
+                      date TEXT NOT NULL, 
+                      time TEXT NOT NULL,
+                      price REAL NOT NULL
+                  )
+                  """)
+        
+        # create reservations table
+        c.execute("""
+                  CREATE TABLE IF NOT EXISTS reservations(
+                      reservation_id INTEGER PRIMARY KEY AUTOINCREMEMT,
+                      passenger_name TEXT NOT NULL,
+                      contact TEXT,
+                      flight_id INTEGER NOT NULL,
+                      created_at TEXT DEFAULT (datetime('now','localtime')),
+                      FOREIGN KEY(flight_id) REFERENCES flights(flight_id) ON DELETE CASCADE
+                  )
+                  """)
+        conn.commit()
